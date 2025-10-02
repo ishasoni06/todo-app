6348768dcf4e82bd5893ccac9fbab8f3aa4d85cb
isha <isha@devscult.awsapps.com>
2025-10-02 10:43:16 +0530

    todoApp

--- components/todo-components/add-todo-input.tsx
+++ components/todo-components/add-todo-input.tsx
@@ -3,32 +3,71 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { useState } from "react";
 
+interface Todos {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-export default function AddTodoInput({todos, setTodos }: { todos: { id: number; title: string; completed: boolean }[], setTodos: (todos: { id: number; title: string; completed: boolean }[]) => void }) {
+interface TodoListProps {
+  todos: Todos[];
+  setTodos: (
+    todos: { id: number; title: string; completed: boolean }[]
+  ) => void;
 
-    const [input, setInput] = useState("");
+  selectId: number;
+  setSelectId: (v: number) => void;
+  input: string;
+  setInput: (v: string) => void;
+  editing: boolean;
+  setEditing: (v: boolean) => void;
+}
+export default function AddTodoInput({
+  todos,
+  setTodos,
+  selectId,
+  setSelectId,
+  input,
+  setInput,
+  editing,
+  setEditing,
+}: TodoListProps) {
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos,
+      { id: todos.length + 1, title: input, completed: false },
+    ]);
+    setInput("");
+  };
+  const handleUpdateTodo = () => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === selectId ? { ...todo, title: input } : todo
+      )
+    );
+    setInput("");
+    setEditing(false);
+    setSelectId(0);
+  };
 
-    const handleAddTodo = () => {
-        setTodos([...todos, { id: todos.length + 1, title: input, completed: false }]);
-        setInput("");
-    }
-
-    return (
-        <div className="flex items-center gap-2 max-w-lg w-full px-5">
-            <Input 
-                className="border-2 border-black h-10 py-6 rounded-3xl" 
-                placeholder="Write your todo here" 
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-            />
-            <Button 
-                variant="outline" 
-                size="icon" 
-                className="h-12 w-12 p-3 rounded-full bg-black border-none text-white hover:border-2 hover:border-black cursor-pointer" 
-                onClick={handleAddTodo}
-            >
-                <Plus size={20} />
-            </Button>
-        </div>
-    )
+  return (
+    <>
+      <div className="flex items-center gap-2 max-w-lg w-full px-5">
+        <Input
+          className="border-2 border-black h-10 py-6 rounded-3xl"
+          placeholder="Write your todo here"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <Button
+          variant="outline"
+          size="icon"
+          className="h-12 w-12 p-3 rounded-full bg-black border-none text-white hover:border-2 hover:border-black cursor-pointer"
+          onClick={editing ? handleUpdateTodo : handleAddTodo}
+        >
+          <Plus size={20} />
+        </Button>
+      </div>
+    </>
+  );
 }
--- components/todo-components/todo-list.tsx
+++ components/todo-components/todo-list.tsx
@@ -1,28 +1,78 @@
-
 import { Edit2Icon, TrashIcon } from "lucide-react";
 import { Checkbox } from "../ui/checkbox";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { todo } from "node:test";
 
-export default function TodoList({ todos, setTodos }: { todos: { id: number; title: string; completed: boolean }[], setTodos: (todos: { id: number; title: string; completed: boolean }[]) => void }) {
+interface Todos {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-    const handleCheckedChange = (id: number) => setTodos(todos.map((todo) => todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+interface onEdit {
+  id: number;
+  title: string;
+}
+interface TodoListProps {
+  todos: Todos[];
+  setTodos: (
+    todos: { id: number; title: string; completed: boolean }[]
+  ) => void;
 
-    const handleDeleteTodo = (id: number) => setTodos(todos.filter((todo) => todo.id !== id));
+  onEdit: any;
+}
 
-    return (
-<div className="flex flex-col gap-2 max-w-lg w-full">
-            {todos.map((todo: { id: number; title: string; completed: boolean }) => (
-                <div key={todo.id} className="flex items-center gap-2 w-full justify-between border-2 border-black py-3 px-5 pb-2 rounded-3xl">
-                    <div className="flex items-center gap-2">
-                    <Checkbox checked={todo.completed} onCheckedChange={() => handleCheckedChange(todo.id)} />
-                    <h1 className={cn("text-lg font-bold", todo.completed && "line-through")}>{todo.title}</h1>
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Edit2Icon />
-                        <TrashIcon onClick={() => handleDeleteTodo(todo.id)} />
-                    </div>
-                </div>
-            ))}
+export default function TodoList({
+  todos,
+  setTodos,
+
+  onEdit,
+}: TodoListProps) {
+  const handleCheckedChange = (id: number) =>
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+
+  const handleDeleteTodo = (id: number) =>
+    setTodos(todos.filter((todo) => todo.id !== id));
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 max-w-lg w-full">
+        {todos.map(
+          (todo: { id: number; title: string; completed: boolean }) => (
+            <div
+              key={todo.id}
+              className="flex items-center gap-2 w-full justify-between border-2 border-black py-3 px-5 pb-2 rounded-3xl"
+            >
+              <div className="flex items-center gap-2">
+                <Checkbox
+                  checked={todo.completed}
+                  onCheckedChange={() => handleCheckedChange(todo.id)}
+                />
+                <h1
+                  className={cn(
+                    "text-lg font-bold",
+                    todo.completed && "line-through"
+                  )}
+                >
+                  {todo.title}
+                </h1>
+              </div>
+              <div className="flex items-center gap-2">
+                <Edit2Icon onClick={() => onEdit(todo.id, todo.title)} />
+
+                <TrashIcon onClick={() => handleDeleteTodo(todo.id)} />
+              </div>
             </div>
-    )
+          )
+        )}
+      </div>
+    </>
+  );
 }
